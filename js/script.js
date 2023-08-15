10edaa88b1be971b6e0e5d32f67adffa714ad523
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2023-08-15 18:57:52 +1000

    Create function for computer to select rand choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("test external Js is linked")
+function getComputerChoice() {
+    const choices = ["rock","paper","scissors"];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerChoice);
+}
+
+getComputerChoice()
