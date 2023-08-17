d3a45e0f3b408c99ffed3e1524e99cb5e32b115b
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2023-08-17 11:52:41 +1000

    Create function for computers random choice

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ let playerScore = 0;
 let computerScore = 0;
 let gameCount = 0;
 
+// generate computers choice 
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1:
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+    }
+}
 
