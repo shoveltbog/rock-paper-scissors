1d57f4be2d488337b8c9f211dea8678421dd13c7
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2024-02-15 01:26:31 +1100

    Update DOM to show results & scoreboard

--- js/script.js
+++ js/script.js
@@ -21,38 +21,27 @@ function getComputerChoice() {
 
 // play a round of the game
 function playRound(playerSelection, computerSelection) {
-    
+    let result;
     if (playerSelection === computerSelection) {
         gameCount++;
-        console.log('Games Played: ' + gameCount + '\n' + 'Players Score: ' + playerScore + '\n' + 'Computers Score: ' + computerScore);
-            return `You Draw! ${playerSelection} draws ${computerSelection}`;
-
-      } else if ((playerSelection === 'ROCK' && computerSelection === 'PAPER') || (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') || (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')) {
+        result = `You Draw! ${playerSelection} draws ${computerSelection}`;
+    } else if ((playerSelection === 'ROCK' && computerSelection === 'PAPER') || (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') || (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')) {
         gameCount++;
         computerScore++;
-        console.log('Games Played: ' + gameCount + '\n' + 'Players Score: ' + playerScore + '\n' + 'Computers Score: ' + computerScore);
-            return  `You Lose! ${computerSelection} beats ${playerSelection}`;
-
-      } else if ((playerSelection ==='ROCK' && computerSelection === 'SCISSORS') || (playerSelection === 'PAPER' && computerSelection === 'ROCK') || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')){
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if ((playerSelection ==='ROCK' && computerSelection === 'SCISSORS') || (playerSelection === 'PAPER' && computerSelection === 'ROCK') || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')){
         gameCount++;
         playerScore++;
-        console.log('Games Played: ' + gameCount + '\n' + 'Players Score: ' + playerScore + '\n' + 'Computers Score: ' + computerScore);
-            return  `You Win! ${playerSelection} beats ${computerSelection}`;
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        result = "Error, Please enter either Rock, Paper, or Scissors & try again";
+    }
 
-      } else {
-            return ("Error, Please enter either Rock, Paper or Scissors & try again");
-      }
+    // Display result in the result div
+    updateResultDisplay(result);
+    updateScoreboard();
 }
-
-// Play 5 rounds of the game
-// function game() {
-//     for (gameCount; gameCount < 5;) {
-//         playerSelection = prompt("ROCK, PAPER or SCISSORS?: ").toUpperCase();
-//         computerSelection = getComputerChoice();
-//         console.log(playRound(playerSelection, computerSelection));
-//     }
-// }
-
+        
 // buttons
 const buttonContainer = document.querySelector('body');
 
@@ -60,30 +49,53 @@ const rockButton = document.createElement('button');
 rockButton.innerText = "ROCK";
 buttonContainer.appendChild(rockButton);
 
-rockButton.addEventListener('click', () => {
-    const playerSelection = "ROCK";
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection)); 
-});
-
 const paperButton = document.createElement('button');
 paperButton.innerText = "PAPER";
 buttonContainer.appendChild(paperButton);
-
-paperButton.addEventListener('click', () => {
-    const playerSelection = "PAPER";
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection)); 
-});
 
 const scissorsButton = document.createElement('button');
 scissorsButton.innerText = "SCISSORS";
 buttonContainer.appendChild(scissorsButton);
 
+// create div for displaying results & give id
+const resultDiv = document.createElement('div');
+resultDiv.id = "resultDiv";
+buttonContainer.appendChild(resultDiv);
+
+// create div for displaying scoreboard
+const scoreboardDiv = document.createElement('div');
+scoreboardDiv.id = "scoreboardDiv";
+buttonContainer.appendChild(scoreboardDiv);
+
+// append new result as new child element of the result div
+function updateResultDisplay(result) {
+    const resultParagraph = document.createElement('p');
+    resultParagraph.textContent = result;
+    resultDiv.appendChild(resultParagraph);
+}
+
+// update scoreboard function
+function updateScoreboard() {
+    const scoreboardContent = `Games Played: ${gameCount}\nPlayers Score: ${playerScore}\nComputers Score: ${computerScore}`;
+    scoreboardDiv.textContent = scoreboardContent;
+}
+
+// Button event listeners
+rockButton.addEventListener('click', () => {
+    const playerSelection = "ROCK";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+
+});
+
+paperButton.addEventListener('click', () => {
+    const playerSelection = "PAPER";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
 scissorsButton.addEventListener('click', () => {
     const playerSelection = "SCISSORS";
     const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection)); 
+    playRound(playerSelection, computerSelection);
 });
-
-// game();
